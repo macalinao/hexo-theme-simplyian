cbc28dc018eb0df64784f346c09974b8173fc11c
Ian Macalinao <me@ian.pw>
2015-01-04 09:04:02 -0600

    Fix disqus embed

--- source/js/main.js
+++ source/js/main.js
@@ -357,12 +357,3 @@ $(document).on({
     setTimeout("$('#comment-box').children().remove();", 100);
   },
 });
-
-var disqus_shortname = 'simplyian';
-(function() {
-  var dsq = document.createElement('script');
-  dsq.type = 'text/javascript';
-  dsq.async = true;
-  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
